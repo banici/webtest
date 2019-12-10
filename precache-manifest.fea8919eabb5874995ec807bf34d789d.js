664652dc4577c919212b1c1538d13a5ed9f5b35c
ille Banic <banicworld@gmail.com>
2019-12-10 16:03:51 +0100

    Updates

---
+++ precache-manifest.fea8919eabb5874995ec807bf34d789d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0573b88d59c6206302bbb8db50538fe",
+    "url": "/webtest/index.html"
+  },
+  {
+    "revision": "7549a150052733307e2e",
+    "url": "/webtest/static/css/main.77373bd9.chunk.css"
+  },
+  {
+    "revision": "f5dfc05e1cf2f2f4a504",
+    "url": "/webtest/static/js/2.6eba9e81.chunk.js"
+  },
+  {
+    "revision": "7549a150052733307e2e",
+    "url": "/webtest/static/js/main.cd0f1745.chunk.js"
+  },
+  {
+    "revision": "ecc748947e49b92c4939",
+    "url": "/webtest/static/js/runtime-main.4f6d58e2.js"
+  },
+  {
+    "revision": "770acd9000f18a5c0fdc4fc70e011cbe",
+    "url": "/webtest/static/media/Thumbs.770acd90.db"
+  },
+  {
+    "revision": "0279db96261d0248a340b64495603adf",
+    "url": "/webtest/static/media/laptop-screen.0279db96.jpg"
+  }
+]);
