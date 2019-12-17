6928c4acff71a04f3d98cfae433394c9914511d1
ille Banic <banicworld@gmail.com>
2019-12-17 05:41:32 +0100

    accepted incoming changes from dev branch

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -2,52 +2,6 @@ import React from 'react';
 import './Form.css';
 
 
-<<<<<<< HEAD
-const Form = (props) => {
-
-
-    const [emailState, setEmailState] = useState({
-        email: [
-            {recipient: ''},
-            {sender: ''},
-            {text: ''}
-        ]
-    })
-
-    const {email} = emailState;
-
-
-    const sendEmail = () => {
-        setEmailState({
-            email: [
-                {recipient: emailState.recipient},
-                {sender: emailState.sender},
-                {text: emailState.text}
-            ]
-        })
-        fetch(`http://127.0.0.1:4000/send-email?recipient=${email.recipient}&sender=${email.sender}&text=${email.text}`) //query string url
-      .catch(err => console.error(err))
-    }
-
-    const buttonFuncs = () => {
-        props.click();
-        sendEmail();
-    }
-
-    return (
-
-        <div className="formWrap">
-            <form id="formContainer" autoComplete="off">
-                <input type="text" className="nameInput" name="name" value={email.recipient}
-                onChange={e => sendEmail({ email: {...email, recipient: e.target.value} })}></input>
-                <label for="name" id="enterName">Name</label>
-                <input type="text" className="emailInput" value={email.sender}
-                onChange={e => sendEmail({ email: {...email, sender: e.target.value} })}></input>
-                <textarea className="msgInput"></textarea>
-                <button type="submit" className="btnSend" onClick={buttonFuncs} value={email.text}
-                onChange={e => sendEmail({ email: {...email, text: e.target.value} })}>send</button>
-            </form>
-=======
 const Form = () => {
     return(
         <div>
@@ -69,14 +23,9 @@ const Form = () => {
 
                            
            
->>>>>>> dev
         </div>
     )
 }
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> dev
 export default Form;
