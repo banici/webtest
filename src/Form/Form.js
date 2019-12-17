c2713d0ce9fdb3261f9deee4290af2f9c4d53bb2
ille Banic <banicworld@gmail.com>
2019-12-17 05:30:44 +0100

    added formspree to Form component

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,56 +1,32 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './Form.css';
-import { send } from 'q';
 
 
-const Form = (props) => {
+const Form = () => {
+    return(
+        <div>
+                <form 
+                action="https://formspree.io/martin.krak@mail.com"
+                method="POST"
+                >
 
+                    <input type="text" className="nameInput" name="Name" ></input>
+                    <label for="name" id="enterName">Name</label>
 
-    const [emailState, setEmailState] = useState({
-        email: [
-            {recipient: ''},
-            {sender: ''},
-            {text: ''}
-        ]
-    })
+                    <input type="text" className="emailInput" name="ReplyTo" />
 
-    const {email} = emailState;
+                    <textarea className="msgInput" name="Message"></textarea>
 
+                    <button type="submit" className="btnSend" >send</button>
 
-    const sendEmail = () => {
-        setEmailState({
-            email: [
-                {recipient: emailState.recipient},
-                {sender: emailState.sender},
-                {text: emailState.text}
-            ]
-        })
-        fetch(`http://127.0.0.1:4000/send-email?recipient=${email.recipient}&sender=${email.sender}&text=${email.text}`) //query string url
-      .catch(err => console.error(err))
-    }
+                </form>
 
-    const buttonFuncs = () => {
-        props.click();
-        sendEmail();
-    }
-
-    return (
-
-        <div className="formWrap">
-            <form id="formContainer" autoComplete="off">
-                <input type="text" className="nameInput" name="name" value={email.recipient}
-                onChange={e => sendEmail({ email: {...email, recipient: e.target.value} })}></input>
-                <label for="name" id="enterName">Name</label>
-                <input type="text" className="emailInput" value={email.sender}
-                onChange={e => sendEmail({ email: {...email, sender: e.target.value} })}></input>
-                <textarea className="msgInput"></textarea>
-                <button type="submit" className="btnSend" onClick={buttonFuncs} value={email.text}
-                onChange={e => sendEmail({ email: {...email, text: e.target.value} })}>send</button>
-            </form>
+                           
+           
         </div>
-    );
+    )
 }
 
-export default Form;
 
-// okej?
+
+export default Form;
