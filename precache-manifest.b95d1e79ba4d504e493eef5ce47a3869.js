65e401b31b73785462230331844302745646e650
banici <banicworld@gmail.com>
2021-05-19 00:27:14 +0200

    Updates

---
+++ precache-manifest.b95d1e79ba4d504e493eef5ce47a3869.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef616fdac5afabbbab0a21f37de763e",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "574971be784a92a0e764",
+    "url": "/myPortfolio/static/css/main.7126803c.chunk.css"
+  },
+  {
+    "revision": "a97429760169e51f5a64",
+    "url": "/myPortfolio/static/js/2.45158541.chunk.js"
+  },
+  {
+    "revision": "574971be784a92a0e764",
+    "url": "/myPortfolio/static/js/main.502ee9c8.chunk.js"
+  },
+  {
+    "revision": "618a535873cb5c8d8918",
+    "url": "/myPortfolio/static/js/runtime-main.598308fc.js"
+  },
+  {
+    "revision": "b07a3f4b900a2545a9ecabc42a08caef",
+    "url": "/myPortfolio/static/media/Sv-presentationMe.b07a3f4b.txt"
+  },
+  {
+    "revision": "b8d575575136d550770e409ea7bb24d3",
+    "url": "/myPortfolio/static/media/Sv-presentationSchool.b8d57557.txt"
+  },
+  {
+    "revision": "103dc681eed5a03ee606af93a0c4b62f",
+    "url": "/myPortfolio/static/media/Sv-utbildningsText.103dc681.txt"
+  },
+  {
+    "revision": "885bc8a42bae731dae3cd53d3c20e965",
+    "url": "/myPortfolio/static/media/Thumbs.885bc8a4.db"
+  },
+  {
+    "revision": "3f3cbbabb954a0637a4e56f63bd4f92f",
+    "url": "/myPortfolio/static/media/Vs-logo1.3f3cbbab.png"
+  },
+  {
+    "revision": "a884e1b49c4135b90d30f797284590fc",
+    "url": "/myPortfolio/static/media/html-logo.a884e1b4.png"
+  },
+  {
+    "revision": "0279db96261d0248a340b64495603adf",
+    "url": "/myPortfolio/static/media/laptop-screen.0279db96.jpg"
+  },
+  {
+    "revision": "50c7557ccd8484374799b9949eb6eae7",
+    "url": "/myPortfolio/static/media/react-logo.50c7557c.png"
+  },
+  {
+    "revision": "73e36495984114690fe3a47beda662a0",
+    "url": "/myPortfolio/static/media/sql-logo.73e36495.png"
+  },
+  {
+    "revision": "fe1c411b3c61b6bb16a54a81aeffeac9",
+    "url": "/myPortfolio/static/media/thskolan1.fe1c411b.png"
+  },
+  {
+    "revision": "66f2b3395d83a43276cf2df7740e9d8d",
+    "url": "/myPortfolio/static/media/webPic.66f2b339.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.d096a1944061f803d34b63283936b0b4.js"
+  "/myPortfolio/precache-manifest.b95d1e79ba4d504e493eef5ce47a3869.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myPortfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
